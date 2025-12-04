ad5aec95f6caec3f8e737b0b4892d96741ac2deb
Jacob D'Onofrio <jhd9316@g.rit.edu>
2025-12-03 22:22:31 -0500

    Test timestamp

---
+++ python_watcher/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import argparse
+import os
+from pathlib import Path
+
+def get_timestamp() -> int:
+    path = argparse.ArgumentParser(description="Get a file's atime Unix timestamp.")
+    path.add_argument("file", type=Path, help="Path to the file")
+    args = path.parse_args()
+
+    file: Path = args.file
+    if not file.exists():
+        raise SystemExit(f"Not found: {file}")
+    if not file.is_file():
+        raise SystemExit(f"Not a regular file: {file}")
+
+    st = os.stat(file, follow_symlinks=False)
+
+    atime_timestamp = int(st.st_atime)
+
+
+def main() -> int:
+    timestamp: int = get_timestamp()
+    print(timestamp)
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
