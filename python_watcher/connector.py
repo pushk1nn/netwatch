80e829a67e99173f1b89ce9ce791241e75fb7db1
. <>
2025-12-03 19:34:41 -0500

    added connector code

--- python_watcher/connector.py
+++ python_watcher/connector.py
@@ -1,10 +1,32 @@
+
 import sqlite3
-con = sqlite3.connect("tutorial.db")
+from datetime import datetime
 
-cur = con.cursor()
+def check_session(database, timestamp):
+    # Convert the input timestamp to a string format for SQL query
+    #timestamp_str = timestamp.timestamp()
 
-def query_from_timestamp(timestamp: int) -> int:
-    res = cur.execute("SELECT name FROM sqlite_master")
-    return res
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
 
-res.fetchone()
+    query = """
+    SELECT c1.ip
+    FROM connections c1
+    JOIN connections c2 ON c1.ip = c2.ip
+        WHERE c1.type = 'CONNECT'
+            AND c2.type = 'DISCONNECT'
+            AND c1.unix_time <= ?
+            AND c2.unix_time >= ?;
+    """
+
+    cursor.execute(query, (timestamp, timestamp))
+    ret = cursor.fetchone()
+    conn.close()
+
+    return ret
+
+# Example Usage
+db_path = '../data.sqlite'
+timestamp_to_check = datetime.fromisoformat('2025-12-02 21:06:45.129551-05:00').timestamp()
+
+print(check_session(db_path, timestamp_to_check))
